f8f7ce0672850545f46a7c0d503445b36ed1066e
Michael Olson <mwolson@member.fsf.org>
2016-08-25 17:00:44 -0700

    Initial working config check

--- test/load_test.sh
+++ test/load_test.sh
@@ -5,3 +5,7 @@
 describe "Load test with sharding"
 
 it "starts nginx"
+
+nginx -t -g "error_log $PWD/scenario/shard_consistent/log/nginx/error.log; pid $PWD/scenario/shard_consistent/run/nginx.pid;" \
+      -c $PWD/scenario/shard_consistent/etc/nginx/nginx.conf \
+      2>&1 | grep -v '/var/log/nginx/error.log'
